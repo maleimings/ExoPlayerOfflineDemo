2c9da68d518ce00b588e052154de277cce8c2078
Maleiming <randy.ma@nagarro.com>
2024-11-21 16:36:59 +0800

    Revert "Upgrade Gradle"
    This reverts commit 0d50c6b3f6301ce28e90ed6b89b9288f4e3dfa8e.

--- app/src/main/java/cn/randyma/exoplayerofflinedemo/ui/download/Item.kt
+++ app/src/main/java/cn/randyma/exoplayerofflinedemo/ui/download/Item.kt
@@ -1,43 +1,13 @@
 package cn.randyma.exoplayerofflinedemo.ui.download
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Item(
     val url: String,
     val drmLicenseUrl: String = "",
     val token: String = "",
     val mimetype: String,
     var updateTimeMs: Long = 0L
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readLong()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(url)
-        parcel.writeString(drmLicenseUrl)
-        parcel.writeString(token)
-        parcel.writeString(mimetype)
-        parcel.writeLong(updateTimeMs)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Item> {
-        override fun createFromParcel(parcel: Parcel): Item {
-            return Item(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Item?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
